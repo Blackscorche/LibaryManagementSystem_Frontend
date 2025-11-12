f935cdde5aa328e225b5b49fe18b9951bdf723a3
John Caleb <Calebjohn3112@gmail.com>
2025-11-12 23:38:39 +0100

    login

--- src/sections/auth/login/LoginPage.jsx
+++ src/sections/auth/login/LoginPage.jsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
     if (email === "" || password === "") {
       toast.error("Please enter email and password");
     } else {
-      axios.post(`https://libsback.up.railway.app/login`, { email, password }, { withCredentials: false })
+      axios.post(`https://libsback.up.railway.app/api/auth/login`, { email, password }, { withCredentials: false })
         .then((response) => {
           // handle success
           if (response.status === 200) {
